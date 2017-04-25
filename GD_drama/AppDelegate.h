39258172139d640345b1dc8b6b390895ca24d96c
haha370104 <137368440@qq.com>
2017-04-25 14:03:32 +0800

    Initial Commit

---
+++ GD_drama/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GD_drama
+//
+//  Created by 罗向宇 on 2017/4/25.
+//  Copyright © 2017年 罗向宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
